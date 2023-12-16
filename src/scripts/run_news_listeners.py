e619553dbd3af893fdd036d9e3837287f423a425
Slayer2084 <oeg2084@oegym.de>
2023-12-16 18:51:39 +0100

    make flake8 happy

--- src/scripts/run_news_listeners.py
+++ src/scripts/run_news_listeners.py
@@ -1,7 +1,6 @@
 from src.listeners.news_listener import NewsListener
 from src.listeners.cnbc.cnbc import get_listener as cnbc_listener
 from src.listeners.guardian.guardian import get_listener as guardian_listener
-from src.listeners.nyt.nyt import get_listener as nyt_listener
 from src.scripts.gcloud_message_broker import publish_event_new_news_article_url
 from src.scripts.database import DataBase
 
@@ -13,14 +12,9 @@ def main():
         article_id = db.add_article(article)
         publish_event_new_news_article_url(article_id)
 
-    nyt_subsections_to_include = ["Politics", "Europe", "Asia Pacific", "Middle East", "Africa", "Australia",
-                                  "Americas",
-                                  "Canada"]
-
     news_listener = NewsListener()
     news_listener.add_listener(cnbc_listener(callback))
     news_listener.add_listener(guardian_listener(callback))
-    # news_listener.add_listener(nyt_listener(print_, nyt_subsections_to_include))
     news_listener.start_listeners()
 
 
